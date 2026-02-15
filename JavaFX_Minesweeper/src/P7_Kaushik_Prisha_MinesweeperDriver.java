4e6adaeaa11168bbf7dc42176298e6567ccf3af3
Prisha Kaushik <prisha.kaushik@gmail.com>
2026-02-15 10:40:22 -0800

    Update P7_Kaushik_Prisha_MinesweeperDriver.java

--- JavaFX_Minesweeper/src/P7_Kaushik_Prisha_MinesweeperDriver.java
+++ JavaFX_Minesweeper/src/P7_Kaushik_Prisha_MinesweeperDriver.java
@@ -1,18 +1,5 @@
 
 
-/*
-Name:       Prisha Kaushik
-Date:       03/19/2025
-Period:     7
-
-Is this lab fully working?  Yes
-
-Create a HW Quiz question that any student who completed this lab would reasonably be expected
-to complete within 5 minutes.
-How do you decide which methods to put in an interface?
-*/
-
-
 
 public class P7_Kaushik_Prisha_MinesweeperDriver {
 
